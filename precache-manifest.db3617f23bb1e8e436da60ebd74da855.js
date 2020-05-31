16f40fd13475952657b1c8418939d0d69e9606a7
Nurik <nurlik120998@gmail.com>
2020-06-01 01:51:08 +0700

    Updates

---
+++ precache-manifest.db3617f23bb1e8e436da60ebd74da855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d9f57edcb3c6c92c01e923ad4ccf54",
+    "url": "/Roscos/index.html"
+  },
+  {
+    "revision": "a120b564d980cbbf4d21",
+    "url": "/Roscos/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "6b1850d20c9b1632b41d",
+    "url": "/Roscos/static/css/main.4cf19c34.chunk.css"
+  },
+  {
+    "revision": "a120b564d980cbbf4d21",
+    "url": "/Roscos/static/js/2.f3bbdc29.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/Roscos/static/js/2.f3bbdc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1850d20c9b1632b41d",
+    "url": "/Roscos/static/js/main.fb55c523.chunk.js"
+  },
+  {
+    "revision": "c0bee12e9e9c5a462e0b",
+    "url": "/Roscos/static/js/runtime-main.3ef5b7d4.js"
+  }
+]);
